88e922adebeaa3603e399bbabc5a88f589ff79ff
amrfahmy <amrfahmy57@gmail.com>
2019-10-03 00:19:20 +0200

    divid pages into partials

---
+++ partials/header.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="icon" href="http://example.com/favicon.png/fa-database">
+
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
+
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Connect Fish</title>
+  </head>
+  <body>
+
+
+
+      <nav class="navbar navbar-expand-sm navbar-light bg-light" data-toggle="affix">
+          <div class="mx-auto d-sm-flex d-block flex-sm-nowrap">
+              <a class="navbar-brand" href="#"><span class="light">Connect</span> Fish <i class="fa fa-database"></i></i></a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample" aria-controls="navbarsExample" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+          </div>
+      </nav>
