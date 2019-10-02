a27098968405ec3c879704be39e57728f8495a3a
amrfahmy <amrfahmy57@gmail.com>
2019-10-02 23:20:03 +0200

    Add html and css

---
+++ index.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="icon" href="http://example.com/favicon.png/fa-database">
+
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
+
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Connect Fish</title>
+  </head>
+  <body>
+
+
+  <nav class="navbar navbar-expand-sm navbar-light bg-light" data-toggle="affix">
+      <div class="mx-auto d-sm-flex d-block flex-sm-nowrap">
+          <a class="navbar-brand" href="#"><span class="light">Connect</span> Fish <i class="fa fa-database"></i></i></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample" aria-controls="navbarsExample" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+      </div>
+  </nav>
+
+  <section class="body">
+
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md left">
+          <h1>Control over the world !</h1>
+          <h5 class="description">Connect Fish helps you to manipulate datasets, sign up to know more.</h5>
+          <div class="center-img">
+            <img class="myimg"  src="images/database.png" alt="">
+          </div>
+        </div>
+        <div class="col-md form">
+          <div class="card">
+            <h3>Sign in</h3>
+            <form id="signinform" class="" action="index.html" method="post">
+              <label for="">Email</label>
+              <input placeholder="email" required type="email" name="" value="">
+              <br>
+              <label for="">Password</label>
+              <input autocomplete="off" type="password" placeholder="password" name="" value="">
+            </form>
+            <button class="btn btn-primary"type="button" name="button">Sign in</button>
+          </div>
+          <hr class="style-one">
+
+          <div class="card">
+            <h3>Sign up</h3>
+
+            <form class="" action="index.html" method="post">
+
+              <label for="">First Name</label>
+              <small>*Required</small> <br>
+              <input type="text" name="" value=""> <br>
+
+              <label for="">Last Name</label> <br>
+              <input type="text" name="" value=""> <br>
+
+              <label for="">E-mail</label>
+              <small>*Required</small> <br>
+              <input type="Emails" name="" value=""> <br>
+
+              <label for="">Password</label>
+              <small>*Required</small> <br>
+              <input  autocomplete="off" type="password" name="" value=""> <br>
+
+              <label for="">Confirm Password</label>
+              <small>*Required</small> <br>
+              <input autocomplete="off" type="password" name="" value=""> <br>
+
+              <label for="">Gender</label>
+              <small>*Required</small> <br>
+              <input style="width:20px;" type="radio" name="gender" value="male"> <label for="">Male</label>
+              <input style="width:20px;" type="radio" name="gender" value="female"> Female
+              <input style="width:20px;" type="radio" name="gender" value="other"> Other
+
+
+              <br>
+              <input style="width:20px;" type="checkbox" name="" value="">
+
+              <small>*Accepted terms and condtitions</small>
+            </form>
+            <button class="btn btn-outline-primary"type="button" name="button">Sign up</button>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+
+  </section>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
